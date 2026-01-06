df4f9fbc661f10c606c728d7c3a01d3ae72c1777
cahyo40 <mdcnugroho@gmail.com>
2026-01-06 09:40:49 +0700

    feat: Implement GetBudgetTransactionUsecase to fetch budget details and delete TransactionDetailUsecase.

--- lib/apps/features/transaction_detail/domain/usecase/get_budget_transaction_usecase.dart
+++ lib/apps/features/transaction_detail/domain/usecase/get_budget_transaction_usecase.dart
@@ -1,11 +1,15 @@
 // import '../repositories/get_budget_transaction_repository.dart';
 
+import 'package:dhuwitku/apps/data/model/budget_model.dart';
+import 'package:dhuwitku/apps/features/transaction_detail/data/repositories/transaction_detail_repository_impl.dart';
+
 class GetBudgetTransactionUsecase {
   // final GetBudgetTransactionRepository _repo;
+  final TransactionDetailRepositoryImpl _repo;
 
-  GetBudgetTransactionUsecase();
+  GetBudgetTransactionUsecase(this._repo);
 
-  Future<void> call() async {
-    // TODO: Implement usecase logic
+  Future<BudgetModel> call(String id) async {
+    return await _repo.getBudgetDetail(id);
   }
 }
