ee49f54de8ce9316aa3918660efbbc5a094d378f
cahyo40 <mdcnugroho@gmail.com>
2026-01-10 08:48:26 +0700

    feat: implement budget amount form screen
    - Replace placeholder with YoTextFormField for amount input.
    - Use IndonesiaCurrencyFormatter.
    - Apply styling.

--- lib/apps/features/budget_create/presentation/view/screen/budget_amount_form_screen.dart
+++ lib/apps/features/budget_create/presentation/view/screen/budget_amount_form_screen.dart
@@ -1,5 +1,8 @@
+import 'package:dhuwitku/apps/core/utils/l10n.dart';
+import 'package:dhuwitku/apps/themes/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:yo_ui/yo_ui.dart';
 
 import '../../controller/budget_create_controller.dart';
 
@@ -8,10 +11,32 @@ class BudgetAmountFormScreen extends GetView<BudgetCreateController> {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Center(
-        child: Text('BudgetAmountFormScreen is working'),
-      ),
+    return YoColumn(
+      margin: YoPadding.vertical16,
+      spacing: YoAdaptive.spacingSm(context),
+      crossAxisAlignment: .start,
+      children: [
+        Center(
+          child: YoText.titleMedium(
+            L10n.t.amount.toUpperCase(),
+            color: gray400,
+            fontWeight: .bold,
+          ),
+        ),
+        YoTextFormField(
+          isRequired: true,
+          textAlign: TextAlign.center,
+          inputStyle: YoInputStyle.filled,
+          focusedBorderColor: context.backgroundColor,
+          fillColor: context.backgroundColor,
+          textStyle: context.yoCurrencyLarge.copyWith(fontSize: 48),
+          controller: controller.amount,
+          inputFormatters: [IndonesiaCurrencyFormatter()],
+          inputType: YoInputType.number,
+          hintText: L10n.t.hint_input_amount,
+          hintStyle: context.yoBodyMedium.copyWith(color: gray400),
+        ),
+      ],
     );
   }
 }
